d69ff6b5c8c0c4b50a3590f1bde40fd679fe14c3
Madhuri Tayade <mt202202@appystack.com>
2023-05-26 18:06:50 +0530

    assignment, while loop, do--while loop

---
+++ src/controlflowstatements/WhileLoopDemo.java
@@ -0,0 +1,12 @@
+package controlflowstatements;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello");
+            i++;
+        }
+    }
+}
+
