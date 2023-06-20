b32d3ffff7194e78e1e98656c0dc7dda7e44a261
Madhuri Tayade <mt202202@appystack.com>
2023-06-20 18:23:37 +0530

    constructor

--- src/oops/OddEvenNo.java
+++ src/oops/OddEvenNo.java
@@ -5,7 +5,7 @@ public class OddEvenNo {
         return n1 + n2;
     }
 
-    void checkOddEven(int no) {
+   public void checkOddEven(int no) {
         if (no % 2 == 0)
             System.out.println("Given no is even");
         else
@@ -18,6 +18,7 @@ class MethodDemo {
     public static void main(String[] args) {
         OddEvenNo oddEvenNo = new OddEvenNo();
         oddEvenNo.checkOddEven(5);
+
         int total = OddEvenNo.addition(5, 10);
         System.out.println("Total " + total);
     }
