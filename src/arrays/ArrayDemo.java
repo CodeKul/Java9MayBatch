7a426f5579c25e6433b05036f421c78b422c14e4
Madhuri Tayade <mt202202@appystack.com>
2023-07-24 17:57:18 +0530

    arrays

---
+++ src/arrays/ArrayDemo.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[1] = 5;
+        for (int i=0;i<5;i++){
+            arr[i] = i+1;
+        }
+        for (int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
