2e1252564ec5030fb30ccc099f0addf35cf1c893
Madhuri Tayade <mt202202@appystack.com>
2023-08-22 18:05:10 +0530

    exception handling introduction

---
+++ src/exceptionhandling/Test.java
@@ -0,0 +1,19 @@
+package exceptionhandling;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println(10/0);
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+
+
+    }
+}
