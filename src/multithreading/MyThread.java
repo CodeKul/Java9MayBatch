fd0704d50428039e838129def9b5ebe211674ccf
Madhuri Tayade <mt202202@appystack.com>
2023-08-31 18:04:10 +0530

    Thread Life Cycle

---
+++ src/multithreading/MyThread.java
@@ -0,0 +1,15 @@
+package multithreading;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        System.out.println("in run method");
+    }
+}
+
+class MyThreadImpl {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
