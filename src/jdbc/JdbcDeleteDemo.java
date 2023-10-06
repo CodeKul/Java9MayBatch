8fde05ba5b1e1044a3bebcb00667a6a75dba9315
Madhuri Tayade <mt202202@appystack.com>
2023-10-06 18:15:43 +0530

    jdbc

--- src/jdbc/JdbcDeleteDemo.java
+++ src/jdbc/JdbcDeleteDemo.java
@@ -5,18 +5,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JdbcUpdateDemo {
+public class JdbcDeleteDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://localhost:3306/Sept25Java";
         String username = "codekul";
         String password = "Codekul@123";
         Connection con = null;
-        String sql = "update emp set sal = ? where empno = ?";
+        String sql = "Delete from emp where empno = ?";
         Class.forName("com.mysql.cj.jdbc.Driver");
         con = DriverManager.getConnection(url,username,password);
         PreparedStatement ps = con.prepareStatement(sql);
-        ps.setDouble(1,15000);
-        ps.setInt(2,7839);
+        ps.setInt(1,8562);
         int n = ps.executeUpdate();
         System.out.println(n +" row/s affected");
         con.close();
